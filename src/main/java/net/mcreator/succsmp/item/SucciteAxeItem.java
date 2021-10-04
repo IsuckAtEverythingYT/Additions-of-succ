2091df723b71b42223e1f3d559fd6bbe59f3f641
IsuckAtEverythingYT <75032354+SucksAtstuff@users.noreply.github.com>
2021-10-04 17:51:41 +0200

    added succite axe texture not working moved all of the mods items into the creative tab of succs additions

--- src/main/java/net/mcreator/succsmp/item/SucciteAxeItem.java
+++ src/main/java/net/mcreator/succsmp/item/SucciteAxeItem.java
@@ -4,49 +4,48 @@ package net.mcreator.succsmp.item;
 import net.minecraftforge.registries.ObjectHolder;
 
 import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.item.SwordItem;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
 import net.minecraft.item.IItemTier;
+import net.minecraft.item.AxeItem;
 
 import net.mcreator.succsmp.itemgroup.SuccsAdditionsItemGroup;
 import net.mcreator.succsmp.SuccsmpModElements;
 
 @SuccsmpModElements.ModElement.Tag
-public class MakkusediteSwordItem extends SuccsmpModElements.ModElement {
-	@ObjectHolder("succsmp:makkusedite_sword")
+public class SucciteAxeItem extends SuccsmpModElements.ModElement {
+	@ObjectHolder("succsmp:succite_axe")
 	public static final Item block = null;
-	public MakkusediteSwordItem(SuccsmpModElements instance) {
-		super(instance, 5);
+	public SucciteAxeItem(SuccsmpModElements instance) {
+		super(instance, 20);
 	}
 
 	@Override
 	public void initElements() {
-		elements.items.add(() -> new SwordItem(new IItemTier() {
+		elements.items.add(() -> new AxeItem(new IItemTier() {
 			public int getMaxUses() {
 				return 5200;
 			}
 
 			public float getEfficiency() {
-				return 15f;
+				return 4f;
 			}
 
 			public float getAttackDamage() {
-				return 8f;
+				return 2f;
 			}
 
 			public int getHarvestLevel() {
-				return 5;
+				return 1;
 			}
 
 			public int getEnchantability() {
-				return 20;
+				return 2;
 			}
 
 			public Ingredient getRepairMaterial() {
-				return Ingredient.fromStacks(new ItemStack(MakkusediteClumpItem.block));
+				return Ingredient.EMPTY;
 			}
-		}, 3, -2f, new Item.Properties().group(SuccsAdditionsItemGroup.tab).isImmuneToFire()) {
-		}.setRegistryName("makkusedite_sword"));
+		}, 1, -3f, new Item.Properties().group(SuccsAdditionsItemGroup.tab).isImmuneToFire()) {
+		}.setRegistryName("succite_axe"));
 	}
 }
--- src/main/java/net/mcreator/succsmp/world/dimension/SuccsPocketDimensionDimension.java
+++ src/main/java/net/mcreator/succsmp/world/dimension/SuccsPocketDimensionDimension.java
@@ -194,7 +194,7 @@ public class SuccsPocketDimensionDimension extends SuccsmpModElements.ModElement
 					pz = pos.getZ() + 0.5 + 0.25 * j;
 					vz = random.nextFloat() * 2 * j;
 				}
-				world.addParticle(ParticleTypes.EXPLOSION, px, py, pz, vx, vy, vz);
+				world.addParticle(ParticleTypes.PORTAL, px, py, pz, vx, vy, vz);
 			}
 			if (random.nextInt(110) == 0)
 				world.playSound(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5,
