e64a132a598621e9bd541df7631822792784c5ab
MakkusuTV <maxhe@DESKTOP-CAV4JBM.fritz.box>
2021-10-06 16:03:32 +0200

    idk what I added ;-;

--- src/main/java/net/mcreator/succsmp/item/SucciteAxeItem.java
+++ src/main/java/net/mcreator/succsmp/item/SucciteAxeItem.java
@@ -1,12 +1,20 @@
 
 package net.mcreator.succsmp.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.AxeItem;
+
+import net.mcreator.succsmp.itemgroup.SuccsAdditionsItemGroup;
+import net.mcreator.succsmp.SuccsmpModElements;
+
 @SuccsmpModElements.ModElement.Tag
 public class SucciteAxeItem extends SuccsmpModElements.ModElement {
-
 	@ObjectHolder("succsmp:succite_axe")
 	public static final Item block = null;
-
 	public SucciteAxeItem(SuccsmpModElements instance) {
 		super(instance, 20);
 	}
@@ -38,8 +46,6 @@ public class SucciteAxeItem extends SuccsmpModElements.ModElement {
 				return Ingredient.EMPTY;
 			}
 		}, 1, -3f, new Item.Properties().group(SuccsAdditionsItemGroup.tab).isImmuneToFire()) {
-
 		}.setRegistryName("succite_axe"));
 	}
-
 }
